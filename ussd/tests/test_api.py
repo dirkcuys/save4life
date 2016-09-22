4f95d3bb954e50836c615979984f9c54d79f5a3e
Dirk Uys <dirkcuys@gmail.com>
2016-09-22 15:53:16 +0200

    Add test

---
+++ ussd/tests/test_api.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from django.test import Client
+
+# Create your tests here.
+class TestUssdApi(TestCase):
+    def test_get_user_info(self):
+        c = Client()
+        resp = c.get('/ussd/user_registration/278312345678/')
+        self.assertEquals(resp.json(), {"msisdn": u"278312345678", "ussd_user_isnone": True})
